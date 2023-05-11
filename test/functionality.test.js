64602ba33cb425f02e21d2445eb4714619d44888
mutugi-c <mutugichris@gmail.com>
2023-05-11 22:33:18 +0200

    Create test for update tasks function
    Add a unit test for the function that updates the status of tasks

--- test/functionality.test.js
+++ test/functionality.test.js
@@ -1,32 +1,32 @@
 import { JSDOM, } from 'jsdom';
 import { addmylist, deletemylist, completedmylist, clearcompletedmylist, editmylist, } from './functionallityTest.js';
-  
-  const dom = new JSDOM('<!doctype html><html><body></body></html>');
-  global.window = dom.window;
-  global.document = window.document;
 
-  const localStorageMock = (() => {
-    let store = {};
-  
-    return {
-      store,
-      getItem: (key) => store[key] || [],
-      setItem: (key, value) => {
-        store[key] = JSON.stringify(value);
-      },
-      removeItem: (key) => {
-        delete store[key];
-      },
-      clear: () => {
-        store = {};
-      },
-    };
-  })();
-  
-  global.localStorage = localStorageMock;
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = dom.window;
+global.document = window.document;
+
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    store,
+    getItem: (key) => store[key] || [],
+    setItem: (key, value) => {
+      store[key] = JSON.stringify(value);
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+global.localStorage = localStorageMock;
 
 describe('addmylist', () => {
-  it('Adding a new object "todo" and save it in Local Storage', () => {
+  it('Adding a new object of tasks and save it in Local Storage', () => {
     const mockDescription = 'Testing Part 1';
     const expectedList = [
       {
@@ -44,7 +44,7 @@ describe('addmylist', () => {
 });
 
 describe('deletemylist', () => {
-  it('Deleting an existing object "todo" from the Local Storage using its index', () => {
+  it('Deleting an existing object task from the Local Storage using its index', () => {
     const removeindex = 1;
     const removeList = [
       {
@@ -62,26 +62,49 @@ describe('deletemylist', () => {
 });
 
 describe('editmylist', () => {
-    beforeEach(() => {
-      document.body.innerHTML = `
+  beforeEach(() => {
+    document.body.innerHTML = `
         <textarea id="text-area-1"></textarea>
         <textarea id="text-area-2"></textarea>
       `;
-    });
-  
-    afterEach(() => {
-      jest.restoreAllMocks();
-    });
-  
-    it('should edit the description of the task with the parameter id', () => {
-      const deleteContents = [{
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('allows one to update the description of the task at a particular index', () => {
+    const deleteContents = [{
+      id: 1,
+      desc: 'EDIT TASK',
+      completed: false,
+    }];
+
+    localStorage.setItem('myList', deleteContents);
+
+    editmylist(1);
+  });
+});
+
+describe('Operations on Completed tasks', () => {
+  describe('completedmylist', () => {
+    it('should toggle completed attribute of a task in the localStorage', () => {
+      const toggleId = 1;
+      const toggleList = [{
         id: 1,
-        desc: 'EDIT TASK',
+        desc: 'Testing Part 1',
         completed: false,
       }];
-  
-      localStorage.setItem('myList', deleteContents);
-  
-      editmylist(1);
+      const localExpected = [{
+        id: 1,
+        desc: 'Testing Part 1',
+        completed: true,
+      }];
+
+      localStorage.setItem('myList', toggleList);
+
+      completedmylist(toggleId);
+      expect(JSON.parse(localStorage.store['myList'])).toStrictEqual(localExpected);
     });
   });
+});
