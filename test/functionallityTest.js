dec93b959e8e3b84a399ac504018b25c6edca657
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-11 11:02:00 -0500

    Function Delete Task
    with an existing "id"

--- test/functionallityTest.js
+++ test/functionallityTest.js
@@ -11,3 +11,16 @@ export const addmylist = (description) => {
     list.push(newTask);
     localStorage.setItem('myList', list);
   };
+  
+  export const deletemylist = (index) => {
+    let list = JSON.parse(localStorage.getItem('myList'));
+    if (list === null) {
+      list = [];
+    }
+    const remove = list.findIndex((task) => task.index === index);
+    list.splice(remove, 1);
+    for (let i = index - 1; i < list.length; i += 1) {
+      list[i].index = i + 1;
+    }
+    localStorage.setItem('myList', JSON.stringify(list));
+  };
