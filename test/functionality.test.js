09607b4cd7a2c7ff09e6f5a5757b2b960b2094c7
mutugi-c <mutugichris@gmail.com>
2023-05-11 18:06:22 +0200

    Implement a mock local storage

--- test/functionality.test.js
+++ test/functionality.test.js
@@ -1,1 +1,21 @@
 import { addmylist, deletemylist } from './functionallityTest.js';
+
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    store,
+    getItem: ((key) => store[key] || []),
+    setItem: ((key, value) => {
+      store[key] = JSON.stringify(value);
+    }),
+    removeItem: ((key) => {
+      delete store[key];
+    }),
+    clear: (() => {
+      store = {};
+    }),
+  };
+})();
+
+global.localStorage = localStorageMock;
